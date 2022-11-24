55d82f97339fbca7e9b95505823232d357d4c0fe
pfan <gemini0525@foxmail.com>
2022-11-24 16:30:07 +0800

    chore: add theme switch

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,10 @@
 import '../styles/globals.css'
+import dynamic from 'next/dynamic'
 import type { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
 
+const DarkModeSwitch = dynamic(() => import('components/DarkModeSwitch'))
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <ChakraProvider><Component {...pageProps} /></ChakraProvider>
+  return <ChakraProvider> <DarkModeSwitch/><Component {...pageProps} /></ChakraProvider>
 }
